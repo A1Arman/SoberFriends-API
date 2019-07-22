3046ef5f8b42d1c620e6239302a3642b94c9070e
Arman Bhimani <Armanbhimani@gmail.com>
2019-07-22 17:18:43 -0500

    Fixed bug

--- src/posts/posts-service.js
+++ src/posts/posts-service.js
@@ -1,10 +1,10 @@
 const PostsService = {
     getAllPosts(knex) {
         return knex
-            .select(knex.raw('distinct on ("posts.id") "posts.post_title", "posts.post_content", "users.first_name", "users.last_name", "likes.post_id", "likes.owner"'))  
             .from('posts')
             .innerJoin('users', 'posts.owner', '=', 'users.id')
             .innerJoin('likes', 'posts.id', '=', 'likes.post_id') 
+            .select(knex.raw('distinct on ("posts.id") "posts.post_title", "posts.post_content", "users.first_name", "users.last_name", "likes.post_id", "likes.owner"'))  
             
     },
     insertPost(knex, newPost) {
